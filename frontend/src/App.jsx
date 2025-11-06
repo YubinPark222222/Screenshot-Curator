f3e8f621be751c491c67cbfb411b1bf9dad47b66
YubinPark222222 <yubin6166@gmail.com>
2025-11-06 15:35:42 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 
 function App() {
+  console.log("App 렌더됨");
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
     </Routes>
-  )
+  );
 }
+
 
 export default App
