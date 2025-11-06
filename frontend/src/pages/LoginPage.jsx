35f25f4e9334dfd56d4ddb2da51bd8e8e0865a88
YubinPark222222 <yubin6166@gmail.com>
2025-11-06 13:49:04 +0900

    Create LoginPage.jsx

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+function LoginPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>로그인 페이지</h1>
+      <input type="text" placeholder="아이디" /><br />
+      <input type="password" placeholder="비밀번호" /><br />
+      <button>로그인</button>
+    </div>
+  )
+}
+
+export default LoginPage
